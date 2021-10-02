0eea00abe6e778c8e9588d452ec303c80e4a7e2e
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 13:15:06 -0700

    add structure to post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,23 @@
 import react from 'react';
+import { Link } from 'react-router-dom';
 
-const Post = props => {
-    return <h1>Post</h1>;
+const Post = ({post}) => {
+    //create style object
+    const div = {
+        textAlign: "center", 
+        border: "3px solid",
+        margin: "10px auto",
+        width: "80%"
+    }
+
+    return (
+        <div style={div}>
+            <Link to={`post/${post.id}`}>
+                <h1>{post.subject}</h1>
+            </Link>
+            <h2>{post.details}</h2>
+        </div>
+    )
 };
 
 export default Post;
