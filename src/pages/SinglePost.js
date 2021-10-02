29dc4980ad2ab43def816ef9995cdf3f95e51dac
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 13:26:05 -0700

    add structure to singlepost page

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-const SinglePost = (props) => {
-  return <h1>Post</h1>;
+const SinglePost = ({posts, match}) => {
+  const id = parseInt(match.params.id);
+  const post = posts.find(post => post.id === id);
+
+  const div = {
+      textAlign: "center",
+      border: "3px solid green",
+      width: "80%",
+      margin: "30px auto"
+  }
+
+  return (
+      <div style={div}>
+        <h1>{post.subject}</h1>
+        <h2>{post.details}</h2>
+        <Link to="/">
+            <button>Go Back</button>
+        </Link>
+      </div>
+  )
 };
 
 export default SinglePost;
