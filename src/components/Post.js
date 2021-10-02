9938667f1783e9bcea3678e7df4d4b72d08ae38f
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 12:06:00 -0700

    add initial components/pages

---
+++ src/components/Post.js
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const Post = props => {
+    return <h1>Post</h1>;
+};
+
+export default Post;
---
+++ src/pages/SinglePost.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SinglePost = (props) => {
+  return <h1>Post</h1>;
+};
+
+export default SinglePost;
