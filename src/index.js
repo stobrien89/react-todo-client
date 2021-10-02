525bb54e50bf0fe1232ba2a3319ac1f98087da9b
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 11:59:27 -0700

    cra cleanup, add src directory structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// Import Milligram for some Default Styling
+import "milligram";
+// Import the BrowserRouter Component and Rename it Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // Wrap our App Component inside router so App and children can use router
+  // Pass the app component into Route to give it access to router props
+  <Router>
+    <React.StrictMode>
+      <Route component={App} />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
