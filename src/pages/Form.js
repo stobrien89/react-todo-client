ad070a5118b585115d84dd44dd65624369f0dc8c
stobrien89 <stobrien@crimson.ua.edu>
2021-10-06 16:31:36 -0700

    add structure to form component

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = (props) => {
-  return <h1>Post</h1>;
+const Form = ({initialTodo, handleSubmit, buttonLabel, history}) => {
+    //Form data state
+    const [formData, setFormData] = useState(initialTodo);
+
+    const handleChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmission = e => {
+        e.preventDefault();
+
+        handleSubmit(formData);
+
+        history.push('/');
+    }
+    return (
+        <form onSubmit={handleSubmission}>
+            <input 
+                type="text"
+                name="subject"
+                onChange={handleChange}
+                value={formData.subject}
+            />
+             <input
+                type="text"
+                onChange={handleChange}
+                value={formData.details}
+                name="details"
+            />
+            <input type="submit" value={buttonLabel} />
+        </form>
+    )
+
 };
 
 export default Form;
